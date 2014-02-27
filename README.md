5a7fe081f3e0d972a07e07f48b4fc719c9a5be02
Thomas Widhalm <widhalmt@widhalm.or.at>
2014-02-27 23:16:37 +0100

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ check_missing_hosts
 ===================
 
 Icinga Plugin to check for hosts not monitored
+
+Introduction
+------------
+
+While it should be one of most important principles when using monitoring that each and every host that should be monitored has to be reported to monitoring team and included into the monitoring software there are teams where this communication is not as reliable as it should be. If you ever "found" a host that was running in your network but missing from your monitoring software, this plugin is for you.
+
+The plugin uses nmap and ping scans to find all hosts on a subnet and check if every one of them is configured to be monitored.
+
+Please note that this is a very "work in progress" plugin. It was developed on site for a customer as a one-shot script to search for hosts and turned into an icinga plugin as a very dirty quick fix. Please do not consider it production ready by now.
+
+Example
+-------
+
+Just call it as icinga user with options -s holding the subnet and -i holding the path of the icinga.cfg . You have to skip the last triple of the subnet.
+
+    $USER1$/check_missing_hosts.sh -s 192.168.23 -i /etc/icinga/icinga.cfg
+
+ToDo
+----
+
+* Support different subnet sizes
+* allow --help instead of -h
---
+++ src/check_missing_hosts.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+## Icinga plugin to check for hosts missing from icinga configuration
+## Thomas Widhalm (C) 2013. This script is released and distributed under the terms of the GNU General Public License.
+
+# tr [:blank:] \\n | sort | uniq -u
+
+### global variables ###
+
+version=0.1
+
+### functions ###
+
+## show_version ##
+
+# simply prints the version
+
+show_version() {
+  echo "Version: ${version}"
+  exit 0
+}
+
+## show_help ##
+
+# print help
+
+show_help() {
+  echo "
+
+Usage:
+  -s first 3 triples of subnet to scan (by now only 255.255.255.0 netmask is supported)
+  -i path to icinga.cfg
+  -h show this help
+  -V print version information
+
+"
+exit 0
+}
+
+
+### reading arguments ###
+
+
+OPTSTR=s:i:Vh
+
+while getopts ${OPTSTR} SWITCHVAR
+do
+  case ${SWITCHVAR} in
+    s) SUBNET_TO_SCAN=${OPTARG};;
+    i) ICINGA_CONFIG=${OPTARG};;
+    h) show_help;;
+    V) show_version;;
+  esac
+done
+
+#SUBNET_TO_SCAN=$1
+IP_RANGE_TO_SCAN=${SUBNET_TO_SCAN}.1-254
+OBJECTS_CACHE=$(grep object_cache_file ${ICINGA_CONFIG} | cut -d= -f2)
+
+### initializing other variables ###
+
+OUTPUT=""
+
+### intial checks ###
+
+NMAP_TOOL=$(which nmap)
+if [[ $? -gt 0 ]]
+then
+  echo "nmap is missing"
+  exit 3
+fi
+
+### main ###
+
+# scan for all hosts in given subnet
+IPS_IN_SUBNET=$(${NMAP_TOOL} -sP ${IP_RANGE_TO_SCAN} | grep report | cut -d\( -f2 | cut -d\) -f1 | tr -d [:alpha:] | tr -d [:blank:] )
+
+# scan objects.cache file for ip addresses in subnet that are already monitored
+IPS_IN_ICINGA=$(grep address ${OBJECTS_CACHE} | grep ${SUBNET_TO_SCAN} | tr -d [:alpha:] | tr -d [:blank:])
+
+# clean list of ip addresses in subnet from blanks and add newlines
+for i in  $IPS_IN_ICINGA
+do
+  IPS_TEMP=$(echo  $IPS_IN_SUBNET | tr [:blank:] \\n | grep -v "^$i$")
+  IPS_IN_SUBNET=$IPS_TEMP
+done
+
+# clean list of ips in subnet from blanks and add newlines and run loop over it
+for i in $( echo $IPS_IN_SUBNET |  tr [:blank:] \\n | sort -n  )
+do
+  HOSTOUTPUT=$(host $i | cut -d' ' -f5)
+  if [[ $? -gt 0 ]]
+  then
+    OUTPUT="${OUTPUT} IP $i can not be resolved"
+  else
+    OUTPUT="${OUTPUT} ${HOSTOUTPUT}"
+  fi
+done
+
+if [[ -z ${HOSTOUTPUT} ]]
+then
+  echo "all hosts in range ${IP_RANGE_TO_SCAN} monitored by Icinga"
+  exit 0
+else
+  echo "missing hosts: ${OUTPUT}"
+  exit 1
+fi
